9e393065f693bc128e554e94379458a42e1873a6
kislaykumarkk <kislaykumarkk@gmail.com>
2016-03-17 11:30:14 +0530

    Update sel.py
    Comments Added

--- sel.py
+++ sel.py
@@ -7,14 +7,21 @@ import sys
 import os
 import pdfkit
 
-
+#Starting Roll No.
 roll = 10400212001
-i = 151 # do it 2 times
+
+#No of iterations
+i = 151
 
 
 
 while i > 0:
+    #Chrome driver to open Browser
+    #Donload from https://sites.google.com/a/chromium.org/chromedriver/downloads
+    #Add correct path for ChromeDriver here
     driver = webdriver.Chrome('/Users/Kislay/Desktop/chromedriver')
+    
+    #URL of the website to Scrap
     driver.get("http://www.wbutech.net/result_odd1516.php")
 
     def find_by_xpath(locator):
@@ -26,32 +33,27 @@ while i > 0:
 
     class FormPage(object):
         def fill_form(self, roll):
+            #Find Form field to enter the roll no
             find_by_xpath('//input[@name = "rollno"]').send_keys(roll)
             
-
             return self # makes it so you can call .submit() after calling this function
 
         def submit(self):
+            #Click Javascript button
+            #Change value for diff semester
             driver.find_element_by_name("sem7").click()
 
-        def save(self):
-            '''driver.find_element_by_name("SGPA")
-            print'''
 
     roll = roll+1
 
-    '''data = {
-        
-        #roll = roll+1
-        'rollno': roll
-        print roll
-        
-    }'''
 
-
-    FormPage().fill_form(roll).submit()    #driver.find_element_by_name("sem7").click()
+    FormPage().fill_form(roll).submit()
+    
+    #Parse HTML source using BeautifulSoup
     soup = BS(driver.page_source)
 
+
+    #Path to store the HTML files
     path = '/Users/Kislay/Desktop/Result/' + str(roll) +'.html'
 
     
@@ -59,8 +61,10 @@ while i > 0:
         for line in soup.prettify('utf-8',):
             f.write(str(line))
 
+    #Path to store the output PDF file
     out = '/Users/Kislay/Desktop/Result/' + str(roll) + '.pdf'
 
+    #Output as PDF
     pdfkit.from_file(path, out)
 
     driver.quit() # closes the webbrowser
